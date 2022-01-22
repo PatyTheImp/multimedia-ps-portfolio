cd9ea2e3568c1560d542db78c4381e778988b1c9
Patrícia Costa <pattycosta91@gmail.com>
2022-01-22 00:37:32 +0000

    modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navbar = document.querySelector(".navbar");
+const boxContainer = document.querySelector(".box-container");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const modalTitle = document.querySelector(".modal-title");
+const modalImg = document.querySelector(".modal-img");
 
 const toggleMenu = function () {
   navbar.classList.toggle("active");
@@ -8,3 +14,21 @@ const toggleMenu = function () {
 
 menuBtn.addEventListener("click", toggleMenu);
 
+const openModal = function (e) {
+  const clicked = e.target.closest(".content");
+  modalTitle.innerHTML = clicked.dataset.title;
+  modalImg.src = `./imagens/gallery-img-${clicked.dataset.img}.jpg`;
+//   modal.classList.remove("hidden");
+  modal.classList.remove("scaledDown");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+//   modal.classList.add("hidden");
+  modal.classList.add("scaledDown");
+  overlay.classList.add("hidden");
+};
+
+boxContainer.addEventListener("click", openModal);
+overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", closeModal);
